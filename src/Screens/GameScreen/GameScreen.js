da0cd1f8d09cd9da0c15ded4d34028201367a87c
Deividasss <kasetukas123@gmail.com>
2023-08-15 19:16:28 +0300

    update

--- src/Screens/GameScreen/GameScreen.js
+++ src/Screens/GameScreen/GameScreen.js
@@ -19,34 +19,34 @@ const GameScreen = () => {
 
     return (
         <div>
-            <h1>{gameCtx.level}</h1>
-            <h1>{gameCtx.money}</h1>
-            {gameCtx.countdown !== null && gameCtx.countdown > 0 && (
-                <div className=" flex justify-center">
-                    <div class="w-[90%] bg-gray-200 rounded-full">
-                        <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${(gameCtx.progress / 5) * 100}%` }}> 45%</div>
+            <div className="grid mt-[15%] place-items-center">
+                {gameCtx.countdown !== null && gameCtx.countdown > 0 && (
+                    <div className=" flex justify-center">
+                        <div class="w-[100%] bg-gray-200 rounded-full">
+                            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${(gameCtx.progress / 5) * 100}%` }}> 45%</div>
+                        </div>
                     </div>
-                </div>
 
-            )}
-            {gameCtx.selectedCar && (
-                <>
-                    <img style={{ height: 120, width: 180 }} src={gameCtx.selectedCar.image} />
-                    <p>Name: {gameCtx.selectedCar.speed}</p>
-                    <p>Name: {gameCtx.selectedCar.model}</p>
+                )}
+                {gameCtx.selectedCar && (
+                    <>
+                        <img style={{ height: 120, width: 180 }} src={gameCtx.selectedCar.image} />
+                        <p>Speed: {gameCtx.selectedCar.speed}</p>
+                        <p>Name: {gameCtx.selectedCar.model}</p>
 
-                </>
-            )}
-            {!gameCtx.countdown && (
-                <button onClick={() => setShowCarsModal(true)}>Select Car</button>
-            )}
-            {!gameCtx.countdown && (
-                <button onClick={() => setShowGameModal(true)}>Start</button>
-            )}
-            <GameModal showGameModal={showGameModal} closeModal={closeModal} />
-            <LoseModal />
-            <WinModal />
-            <CarsModal showCarsModal={showCarsModal} closeModal={closeModal} />
+                    </>
+                )}
+                {!gameCtx.countdown && (
+                    <button onClick={() => setShowCarsModal(true)}>Select Car</button>
+                )}
+                {!gameCtx.countdown && (
+                    <button onClick={() => setShowGameModal(true)}>Start</button>
+                )}
+                <GameModal showGameModal={showGameModal} closeModal={closeModal} />
+                <LoseModal />
+                <WinModal />
+                <CarsModal showCarsModal={showCarsModal} closeModal={closeModal} />
+            </div>
         </div>
     )
 }
