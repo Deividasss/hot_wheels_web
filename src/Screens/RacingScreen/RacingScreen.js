293690b9cabb123c5ecb83cae72cacad373a2caf
Deividasss <kasetukas123@gmail.com>
2023-09-23 21:24:52 +0300

    update

--- src/Screens/RacingScreen/RacingScreen.js
+++ src/Screens/RacingScreen/RacingScreen.js
@@ -8,16 +8,22 @@ const RacingScreen = () => {
 
     return (
         <div>
-            <p className='collectionTitle w-full my-2 text-[55px] sm:text-[90px] text-center animate__animated animate__pulse animate__slow animate__infinite'>Racing...</p>
-            <div className="ms:flex justify-center mt-10 align-middle">
-                <img className='w-[300px] h-[180px]' src={gameCtx.selectedCar.image}></img>
-                <p className='collectionTitle my-2 mx-5 text-[55px] sm:text-[70px] text-center'>VS</p>
-                <img className='w-[300px] h-[180px]' src={gameCtx.selectedCar.image}></img>
+            <p className='collectionTitle w-full my-2 text-[90px] sm:text-[90px] text-center animate__animated animate__pulse animate__slow animate__infinite'>Racing...</p>
+            <div className="sm:flex justify-center mt-10 align-middle">
+                <div>
+                    <img className='w-[130px] m-auto h-[70px] sm:w-[300px] sm:h-[180px]' src={gameCtx.selectedCar.image}></img>
+                    <p className='collectionTitle my-2 mx-5 text-[25px] sm:text-[35px] text-center'>Deividas</p>
+                </div>
+                <p className='collectionTitle my-2 mx-5 text-[90px] sm:text-[70px] text-center'>VS</p>
+                <div>
+                    <img className='w-[130px] m-auto h-[70px] sm:w-[300px] sm:h-[180px]' src={gameCtx.selectedCar.image}></img>
+                    <p className='collectionTitle my-2 mx-5 text-[25px] sm:text-[35px] text-center'>Deividas</p>
+                </div>
             </div>
             <div className='mt-10 mb-24'>
                 <p className='collectionTitle w-full mt-10 text-[30px] md:text-[25px] lg:text-[40px] text-center'>Time Left: {gameCtx.countdown}</p>
                 <div className=" flex justify-center">
-                    <div class="w-[50%] bg-gray-200 rounded-full">
+                    <div class=" w-[80%] sm:w-[50%] bg-gray-200 rounded-full">
                         <div class="gradient text-xs font-medium text-blue-100 text-center p-3 leading-none rounded-full" style={{ width: `${(gameCtx.progress / 10) * 100}%` }}></div>
                     </div>
                 </div>
--- src/components/Modals/WinModal.js
+++ src/components/Modals/WinModal.js
@@ -39,13 +39,9 @@ const WinModal = () => {
                         >
                             <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                    <div className="sm:flex sm:items-start">
-                                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                            <div className="mt-2">
-                                                <p className="text-sm text-gray-500">
-                                                    YOU WON!!!
-                                                </p>
-                                            </div>
+                                    <div className="mt-3 text-center">
+                                        <div className="mt-2">
+                                            <p className="collectionTitle my-2 mx-5 text-[20px] sm:text-[70px] text-center">YOU WON!!!</p>
                                         </div>
                                     </div>
                                 </div>
